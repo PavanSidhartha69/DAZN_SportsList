6f5fd0fbc1d62bab130843bec81b845aca847feb
Pavan.Sidhartha <Pavan.Sidhartha@dazn.com>
2023-04-20 20:14:15 +0530

    "Splash Screen Added"

---
+++ app/src/main/java/com/example/daznsportslist/SplashScreen.kt
@@ -0,0 +1,26 @@
+package com.example.daznsportslist
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.view.WindowManager
+import com.example.daznsportslist.R
+import kotlinx.coroutines.delay
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen2)
+        supportActionBar?.hide()
+        Log.i("msg","asdad")
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        }, 3000)
+    }
+}
+
+
