68fa4ceeac5bf0049e0af367ede4306340eba4e6
Pavan.Sidhartha <Pavan.Sidhartha@dazn.com>
2023-04-21 12:59:38 +0530

    "Recycler View added with a sample list and background changed to black and white"

--- app/src/main/java/com/example/daznsportslist/Adapter_RV.kt
+++ app/src/main/java/com/example/daznsportslist/Adapter_RV.kt
@@ -2,27 +2,30 @@ package com.example.daznsportslist
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView.Adapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 
-class Adapter_RV () : Adapter<Adapter_RV.rv_ViewHolder>() {
+class Adapter_RV(val playerList: List<String>) : Adapter<Adapter_RV.rv_ViewHolder>() {
 
 
     class rv_ViewHolder(itemView: View) : ViewHolder(itemView) {
+        val playerName = itemView.findViewById<TextView>(R.id.txtTitle)
+        val playerDescription = itemView.findViewById<TextView>(R.id.txtDescription)
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): rv_ViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val view = inflater.inflate(R.layout.item_view,parent,false)
+        val view = inflater.inflate(R.layout.item_view, parent, false)
         return rv_ViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return playerList.size
     }
 
     override fun onBindViewHolder(holder: rv_ViewHolder, position: Int) {
-        holder
+        holder.playerName.text = playerList[position]
     }
 }
--- app/src/main/java/com/example/daznsportslist/MainActivity.kt
+++ app/src/main/java/com/example/daznsportslist/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.example.daznsportslist
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.example.daznsportslist.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var adapter: Adapter_RV
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val randomList : List<String> = listOf("HWLL","ASDAD","gfdgfdg","sdfsfewweffew","asdfsadsadadwqq","SFDDSFDSFsf","AFEEFAFSF","sfdsfdsfdsfds","HWLL","ASDAD","gfdgfdg","sdfsfewweffew","asdfsadsadadwqq","SFDDSFDSFsf","AFEEFAFSF","sfdsfdsfdsfds")
+
+        val list = findViewById<RecyclerView>(R.id.playerList)
+        adapter=Adapter_RV(randomList)
+        list.adapter=adapter
+        list.layoutManager=LinearLayoutManager(this)
     }
 }
