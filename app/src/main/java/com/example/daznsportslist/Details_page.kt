9b643872be8871b384445a4aa3076852879903e0
Pavan.Sidhartha <pavan.sidhartha@dazn.com>
2023-04-23 03:21:05 +0530

    Changed UI & added CardView

--- app/src/main/java/com/example/daznsportslist/Details_page.kt
+++ app/src/main/java/com/example/daznsportslist/Details_page.kt
@@ -1,14 +1,37 @@
 package com.example.daznsportslist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.adapters.TextViewBindingAdapter.setText
 
 class Details_page : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        lateinit var button: Button
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.list_item)
+        val text = findViewById<TextView>(R.id.textView)
+        val Description = intent.getStringExtra("Description")
+        text.setText(Description)
+
+        button = findViewById(R.id.bt_share)
+
+
+
+        button.setOnClickListener({
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "text/plain"
+            intent.putExtra("Share this", Description)
+
+            val chooser = Intent.createChooser(intent, "Share using..")
+            startActivity(chooser)
+        })
 
     }
 }
--- app/src/main/java/com/example/daznsportslist/SportsListAdapter.kt
+++ app/src/main/java/com/example/daznsportslist/SportsListAdapter.kt
@@ -2,13 +2,15 @@ package com.example.daznsportslist
 
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.Adapter
 import com.example.daznsportslist.databinding.ItemViewBinding
 
-class SportsListAdapter(val playerList: List<Tiles>, val context: Context) : Adapter<SportsListAdapter.rv_ViewHolder>() {
+class SportsListAdapter(val playerList: List<Tiles>, val context: Context) :
+    Adapter<SportsListAdapter.rv_ViewHolder>() {
     class rv_ViewHolder(val binding: ItemViewBinding?) : RecyclerView.ViewHolder(binding!!.root) {
     }
 
@@ -22,10 +24,14 @@ class SportsListAdapter(val playerList: List<Tiles>, val context: Context) : Ada
     }
 
     override fun onBindViewHolder(holder: rv_ViewHolder, position: Int) {
-        holder.binding!!.playerName = playerList[position].Label
-
-        holder.binding.Goto.setOnClickListener {
-            val intent = Intent(context,Details_page::class.java)
+        Log.i("msg", playerList[position].toString())
+        holder.binding!!.playerName = playerList[position].Title
+//        holder.binding!!.playerDescription = playerList[position].Description
+        holder.binding.cardView
+            .setOnClickListener {
+            val intent = Intent(context, Details_page::class.java)
+            intent.putExtra("Description", playerList[position].Description.toString())
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             context.startActivity(intent)
         }
     }
