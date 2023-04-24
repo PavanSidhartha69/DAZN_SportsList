7c9c6d06526382f1edbad526c2d57056e26514d5
Pavan.Sidhartha <Pavan.Sidhartha@dazn.com>
2023-04-24 20:38:43 +0530

    "adjusted the functionality of share button and did a few minor UI changes"

--- app/src/main/java/com/example/daznsportslist/Image.kt
+++ app/src/main/java/com/example/daznsportslist/Image.kt
@@ -3,5 +3,7 @@ import com.google.gson.annotations.SerializedName
 data class Image(
     @SerializedName("ImageMimeType" ) var ImageMimeType : String? = null,
     @SerializedName("ImageType"     ) var ImageType     : String? = null,
-    @SerializedName("Id"            ) var Id            : String? = null
+    @SerializedName("Id"            ) var Id            : String? = null,
+    @SerializedName("URL"            ) var URL            : String? = "https://image.discovery.indazn.com/jp/v3/jp/none/111572549752_image-header_pRow_1620527394000/fill/center/top/none/85/1042/828/jpg/image"
+
 )
--- app/src/main/java/com/example/daznsportslist/SportsListAdapter.kt
+++ app/src/main/java/com/example/daznsportslist/SportsListAdapter.kt
@@ -2,12 +2,15 @@ package com.example.daznsportslist
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.Adapter
 import com.example.daznsportslist.databinding.ItemViewBinding
+import java.net.URL
+
 
 class SportsListAdapter(val playerList: List<Tiles>, val context: Context) :
     Adapter<SportsListAdapter.rv_ViewHolder>() {
@@ -26,10 +29,21 @@ class SportsListAdapter(val playerList: List<Tiles>, val context: Context) :
     override fun onBindViewHolder(holder: rv_ViewHolder, position: Int) {
         Log.i("msg", playerList[position].toString())
         holder.binding!!.playerName = playerList[position].Title
-//        holder.binding!!.playerDescription = playerList[position].Description
-        holder.binding.Goto.setOnClickListener {
+        holder.binding!!.playerLabel=playerList[position].Label
+//        try {
+//            val newurl = URL(playerList[position].Image?.URL)
+//            val mIcon_val = BitmapFactory.decodeStream(newurl.openConnection().getInputStream())
+//            Log.i("IMAGE FROM URL","sda")
+//            holder.binding!!.CardImg.setImageBitmap(mIcon_val)
+//        }
+//        catch (e:Exception){
+//            Log.i("EXCEPT",e.toString())
+//        }
+////        holder.binding!!.playerDescription = playerList[position].Description
+        holder.binding.cardView.setOnClickListener {
             val intent = Intent(context, Details_page::class.java)
             intent.putExtra("Description", playerList[position].Description.toString())
+            intent.putExtra("Title",playerList[position].Title.toString())
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             context.startActivity(intent)
         }
